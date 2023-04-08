fe2533702170dd0ead66dc2a23c6787d8444fdd3
mmarsy <117351157+mmarsy@users.noreply.github.com>
2023-04-08 12:39:15 +0200

    added choosing the cheapest card

--- main.py
+++ main.py
@@ -15,15 +15,21 @@ def minimize_deck_price(decklist):
     deck = merge_number_dicts(deck.main, deck.side)
     prices = import_price_data(to_print=False)
     deck_price = 0
+    choices = {}
 
     # main part
     for card in deck:
-        card_price = min([prices[_id] for _id in card.ids])
-        deck_price += card_price * deck[card]
+        card_prices = {_id: prices[_id] for _id in card.ids}
+        card_id = min(card_prices, key=card_prices.get)
+
+        choices[card_id] = deck[card]
+        deck_price += card_prices[card_id] * deck[card]
 
     # return
     print(deck_price)
-    return deck_price
+    for i in choices:
+        print(f'{i}: {choices[i]}')
+    return deck_price, choices
 
 
 def main():
